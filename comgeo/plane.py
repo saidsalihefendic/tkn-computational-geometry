204b8f8e8e423cea244b724af11552f8a8e8b2ef
saidsalihefendic@gmail.com <saidsalihefendic@gmail.com>
2020-10-28 01:23:52 +0100

    Added an exercise for testing if the point is in simple polygon, added SimplePolygon class

--- comgeo/plane.py
+++ comgeo/plane.py
@@ -1,3 +1,6 @@
+from functools import partial
+from math import acos
+
 class Point2D():
     def __init__(self, x, y):
         self.x = x
@@ -9,11 +12,20 @@ class Point2D():
         
         return self.x < other.x
 
-    def __add__(self, point):
-        return Point2D(self.x + point.x, self.y + point.y)
-
     def __eq__(self, other):
         return self.x == other.x and self.y == other.y
+    
+    def __le__(self, other):
+        return self < other or self == other
+
+    def __ge__(self, other):
+        return not self < other
+
+    def __gt__(self, other):
+        return not self <= other
+
+    def __add__(self, point):
+        return Point2D(self.x + point.x, self.y + point.y)
 
     def __neq__(self, other):
         return self.x != other.x or self.y != other.y
@@ -73,7 +85,32 @@ class DirectedEdge():
             return 0
         else:
             return -1
+
+    def contains_point(self, point):
+        """Tests whether a point lies on the directed edge"""
+        if self.orientation(point) == 0:
+            return point >= min(self.begin, self.end) and point <= max(self.begin, self.end)
+
+        return False
+    
+    def is_collinear(self, directed_edge):
+        """Tests whether the directed edge is collinear with another directed edge"""
+
+        return self.orientation(directed_edge.begin) == 0 and self.orientation(directed_edge.end)
+
+    def is_intersecting(self, directed_edge):
+        """Tests whether the directed edge intersects with another edge"""
+        begin_orientation = self.orientation(directed_edge.begin)
+        end_orientation = self.orientation(directed_edge.end)
         
+        if(begin_orientation == 0 or end_orientation == 0):
+            return self.contains_point(directed_edge.begin) or self.contains_point(directed_edge.end) or directed_edge.contains_point(self.begin) or directed_edge.contains_point(self.end)
+
+        directed_begin_orientation = directed_edge.orientation(self.begin)
+        directed_end_orientation = directed_edge.orientation(self.end)
+
+        return begin_orientation != end_orientation and directed_begin_orientation != directed_end_orientation
+    
     def __eq__(self, end):
         return self.begin == self.end
     
@@ -82,3 +119,86 @@ class DirectedEdge():
 
     def __repr__(self):
         return "DirectedEdge({}, {})".format(str(self.begin), (self.end))
+
+
+def get_angle_and_distance(point_1, point_2, point_3):
+    """Returns the angle of < point_1, point_2, point_3"""
+    if point_1 == point_2 or point_1 == point_3 or point_2 == point_3:
+        return 0, 0
+    
+    v1 = Vector2D(point_2, point_1)
+    v2 = Vector2D(point_2, point_3)
+    
+    cosalpha = Vector2D.dot(v1, v2) / (v1.norm() * v2.norm())
+
+    return (acos(cosalpha), v1.norm())
+
+
+class SimplePolygon():
+    def __init__(self, vertices):
+        self.vertices = SimplePolygon.sort(vertices)
+
+    def get_reference_point(self):
+        leftmost_point = self.vertices[0]
+        return Point2D(leftmost_point.x - 1, leftmost_point.y)
+
+    def is_in_polygon(self, point):
+        """Returns whether a point is inside the simple polygon or not"""
+        reference_point = self.get_reference_point()
+        
+        reference_segment = DirectedEdge(point, reference_point)
+
+        num_crossings = 0
+        
+        left_idx = 0
+        while left_idx != len(self):
+            right_idx = (left_idx + 1) % len(self)
+            
+            are_crossing = False
+            
+            if reference_segment.contains_point(self[left_idx]):
+                while reference_segment.contains_point(self[right_idx]):
+                    right_idx = (right_idx + 1) % len(self)
+                    
+                    if right_idx == left_idx:
+                        break
+                
+                if left_idx == right_idx:
+                    left_idx = len(self)
+                    continue
+                
+                left_endpoint = self[(left_idx - 1) % len(self)]
+                right_endpoint = self[(right_idx + 1) % len(self)]
+                
+                are_crossing = reference_segment.orientation(left_endpoint) != reference_segment.orientation(right_endpoint)
+                left_idx = (right_idx + 1) % len(self) if left_idx < (right_idx + 1) % len(self) else len(self) 
+            elif reference_segment.contains_point(self[right_idx]):
+                left_idx += 1
+                continue
+            else:
+                edge = DirectedEdge(self[left_idx], self[right_idx])
+                are_crossing = reference_segment.is_intersecting(edge)
+                left_idx += 1
+            
+            num_crossings = num_crossings + 1 if are_crossing else num_crossings
+            
+        return num_crossings % 2 == 1
+
+    def __len__(self):
+        return len(self.vertices)
+    
+    def __getitem__(self, i):
+        return self.vertices[i] 
+
+    @staticmethod
+    def sort(points):
+        """Given a list of Point2D, returns the clockwise sorted list that forms simple polygon"""
+        if len(points) == 0:
+            return []
+        
+        starting_vertex = min(points)
+        reference_point = starting_vertex + Point2D(0, 1)
+        
+        return sorted(points, key=partial(
+            get_angle_and_distance, point_2=starting_vertex, point_3=reference_point
+        ))
